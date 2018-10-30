577b678bbba7edbb9f99301e536270cda29022c1
klx3300 <inc19991@gmail.com>
2018-10-31 00:00:28 +0800

    added filepath constants

---
+++ engine_race/task/task.cc
@@ -0,0 +1,16 @@
+#include "task.h"
+
+using namespace std;
+using namespace polar_race;
+
+void HeartBeater(string sendaddr){
+    
+}
+
+void HeartBeatChecker(string recvaddr){
+    
+}
+
+void RequestProcessor(string recvaddr){
+    
+}
--- engine_race/task/task.h
+++ engine_race/task/task.h
@@ -11,8 +11,13 @@ namespace polar_race {
 	extern bool *CommitCompletionQueue;
 	extern uint64_t WrittenIndex;
 	extern char *InternalBuffer;
+    extern const std::string VALUES_PATH;
+    extern const std::string INDECIES_PATH;
 
     extern bool ExitSign; // on true, execute exit clean proc
+
+    const std::string HB_ADDR = "\0ralopdb/heartbeat";
+    const std::string REQ_ADDR_PREFIX = "\0ralopdb/request/";
 
     void RequestProcessor(std::string recvaddr);
     void HeartBeater(std::string sendaddr);
