d15a2f6a4ba667e15628c0bdcd43507ee4cb2460
klx3300 <inc19991@gmail.com>
2018-11-02 18:11:36 +0800

    solved silly IPC problem

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -78,7 +78,7 @@ namespace polar_race {
             // parent
             qLogInfo("Startup: FORK completed.");
             qLogInfo("Startup: wait ReqHandler startup complete.");
-            while (start_ok == false);
+            sleep(3);
             qLogInfo("Startup: HeartBeat thread.");
             thread hbthread(HeartBeater, HB_ADDR, &running);
             hbthread.detach();
