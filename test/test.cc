52b7daf974fa0250ed37291007a5487fda6eb7a3
klx3300 <inc19991@gmail.com>
2018-11-03 19:39:36 +0800

    check filelock problem

--- test/test.cc
+++ test/test.cc
@@ -240,7 +240,7 @@ int main() {
     // Write
     auto writeStart = std::chrono::high_resolution_clock::now();
 
-    unsigned numWrite = 10000;
+    unsigned numWrite = 100;
     std::vector<std::thread> writers;
     for (int i = 0; i < numThreads; ++i) {
         writers.emplace_back(std::thread(write, engine, std::ref(keys), numWrite));
@@ -265,7 +265,7 @@ int main() {
     // Random Read
     auto rreadStart = std::chrono::high_resolution_clock::now();
 
-    unsigned numRead = 10000;
+    unsigned numRead = 10;
     std::vector<std::thread> rreaders;
     for (int i = 0; i < numThreads; ++i) {
         rreaders.emplace_back(std::thread(randomRead, engine, std::cref(keys), numRead));
