fbe0bc0bf64914f45e69b8a5685ab73ca1b9ac8b
klx3300 <inc19991@gmail.com>
2018-11-10 18:51:03 +0800

    some consts and types

--- engine_race/commu/commu.h
+++ engine_race/commu/commu.h
@@ -21,6 +21,7 @@ namespace polar_race {
         const uint8_t TYPE_RD = 0;
         const uint8_t TYPE_WR = 1;
         const uint8_t TYPE_OK = 0;
+        const uint8_t TYPE_BUSY = 1;
         const uint8_t TYPE_EEXIST = -1;
     };
     struct RequestResponse {
