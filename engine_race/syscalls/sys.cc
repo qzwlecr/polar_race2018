b2f246041be83dec2cee46df31265e3c739c8d04
klx3300 <inc19991@gmail.com>
2018-11-16 02:01:26 +0800

    fixed NULL not found problem

--- engine_race/syscalls/sys.cc
+++ engine_race/syscalls/sys.cc
@@ -6,7 +6,7 @@ int sys_getaff(pid_t pid, int* cpuid){
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     int rv = sched_getaffinity(pid, sizeof(cpu_set_t), &cpuset);
-    if(rv == -1 || cpuid == NULL) return rv;
+    if(rv == -1 || cpuid == nullptr) return rv;
     if(CPU_COUNT(&cpuset) != 1) return rv;
     for(int i = 0; i < *cpuid; i++){
         if(CPU_ISSET(i, &cpuset)){
