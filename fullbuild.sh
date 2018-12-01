1a03e16c534240be39bb4890414b2eef00dab054
klx3300 <inc19991@gmail.com>
2018-12-02 03:30:29 +0800

    written garbage bash script

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Building engine library (libengine.a)
+echo "Building engine library (libengine.a)"
 make 2>&1
 cd test
-echo Building test program (linking)
+echo "Building test program (linking)"
 make 2>&1
